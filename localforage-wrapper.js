7fcc086ca16ecaf93499af5f9ac944309ec932b0
hybrisCole <jcole@capilleira.com>
2015-08-04 08:19:00 -0600

    Adding json array driver to make this work on safaris' private browsing

--- localforage-wrapper.js
+++ localforage-wrapper.js
@@ -6,7 +6,7 @@ angular
     localforage.defineDriver(JSONArrayDriver.generateConfig());
     //is it IE?
     var msie,
-      driverConfig = [localforage.INDEXEDDB, localforage.WEBSQL, localforage.LOCALSTORAGE],
+      driverConfig = ['jsonArrayWrapper'],
       CONSTANT_VARS = {
         DATE_FORMAT: 'MM/DD/YYYY HH:mm:ss',
         LOCALFORAGE_EXPIRATION: {
