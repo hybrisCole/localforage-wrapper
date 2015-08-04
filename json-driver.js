1396fd9a936c9bf4ef5f9c3d331fb323c4a7d58d
hybrisCole <jcole@capilleira.com>
2015-08-04 08:30:01 -0600

    Adding json array driver to make this work on safaris' private browsing

--- json-driver.js
+++ json-driver.js
@@ -10,9 +10,11 @@ angular
           _driver: 'jsonArrayWrapper',
           _support: true,
           _initStorage: function() {
+            console.log(1);
             jsonArray = [];
           },
           clear: function(callback) {
+            console.log(2);
             var defer = $q.defer();
             jsonArray = [];
             defer.resolve(jsonArray);
@@ -20,6 +22,7 @@ angular
             return defer.promise;
           },
           getItem: function(key, callback) {
+            console.log(3);
             var defer = $q.defer(),
               item = _.find(jsonArray, {key: key});
             defer.resolve(item);
@@ -28,21 +31,26 @@ angular
 
           },
           iterate: function() {
+            console.log(4);
             // Custom implementation here...
           },
           key: function() {
+            console.log(5);
             // Custom implementation here...
           },
           keys: function() {
+            console.log(6);
             // Custom implementation here...
           },
           length: function(callback) {
+            console.log(7);
             var defer = $q.defer();
             defer.resolve(jsonArray.length);
             callback(null, jsonArray);
             return defer.promise;
           },
           removeItem: function(key, callback) {
+            console.log(8);
             var defer = $q.defer();
             jsonArray = _.difference(jsonArray, [_.find(jsonArray, {key: key})]);
             defer.resolve(jsonArray);
@@ -50,6 +58,7 @@ angular
             return defer.promise;
           },
           setItem: function(key, value, callback) {
+            console.log(9);
             var defer = $q.defer();
             jsonArray.push({
               key: key,
