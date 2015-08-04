3e8c5a59b5c1bd4ae399173a29150c2d972301f0
hybrisCole <jcole@capilleira.com>
2015-08-04 08:03:51 -0600

    Adding json array driver to make this work on safaris' private browsing

--- json-driver.js
+++ json-driver.js
@@ -5,53 +5,57 @@ angular
   .factory('JSONArrayDriver', function($q) {
     var jsonArray = [];
     return {
-      _driver: 'jsonArrayWrapper',
-      _initStorage: function() {
-        jsonArray = [];
-      },
-      clear: function(callback) {
-        var defer = $q.defer();
-        jsonArray = [];
-        defer.resolve(jsonArray);
-        callback(null, jsonArray);
-        return defer.promise;
-      },
-      getItem: function(key, callback) {
-        var defer = $q.defer(),
-          item = _.find(jsonArray, {key: key});
-        defer.resolve(item);
-        callback(null, item);
-        return defer.promise;
+      generateConfig: function() {
+        return {
+          _driver: 'jsonArrayWrapper',
+          _initStorage: function() {
+            jsonArray = [];
+          },
+          clear: function(callback) {
+            var defer = $q.defer();
+            jsonArray = [];
+            defer.resolve(jsonArray);
+            callback(null, jsonArray);
+            return defer.promise;
+          },
+          getItem: function(key, callback) {
+            var defer = $q.defer(),
+              item = _.find(jsonArray, {key: key});
+            defer.resolve(item);
+            callback(null, item);
+            return defer.promise;
 
-      },
-      key: function() {
-        // Custom implementation here...
-      },
-      keys: function() {
-        // Custom implementation here...
-      },
-      length: function(callback) {
-        var defer = $q.defer();
-        defer.resolve(jsonArray.length);
-        callback(null, jsonArray);
-        return defer.promise;
-      },
-      removeItem: function(key, callback) {
-        var defer = $q.defer();
-        jsonArray = _.difference(jsonArray, [_.find(jsonArray, {key: key})]);
-        defer.resolve(jsonArray);
-        callback(null, jsonArray);
-        return defer.promise;
-      },
-      setItem: function(key, value, callback) {
-        var defer = $q.defer();
-        jsonArray.push({
-          key: key,
-          value: value
-        });
-        defer.resolve(jsonArray);
-        callback(null, jsonArray);
-        return defer.promise;
+          },
+          key: function() {
+            // Custom implementation here...
+          },
+          keys: function() {
+            // Custom implementation here...
+          },
+          length: function(callback) {
+            var defer = $q.defer();
+            defer.resolve(jsonArray.length);
+            callback(null, jsonArray);
+            return defer.promise;
+          },
+          removeItem: function(key, callback) {
+            var defer = $q.defer();
+            jsonArray = _.difference(jsonArray, [_.find(jsonArray, {key: key})]);
+            defer.resolve(jsonArray);
+            callback(null, jsonArray);
+            return defer.promise;
+          },
+          setItem: function(key, value, callback) {
+            var defer = $q.defer();
+            jsonArray.push({
+              key: key,
+              value: value
+            });
+            defer.resolve(jsonArray);
+            callback(null, jsonArray);
+            return defer.promise;
+          }
+        };
       }
     };
   });
